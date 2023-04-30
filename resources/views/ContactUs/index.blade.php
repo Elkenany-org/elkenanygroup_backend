37d6606dc4618d3647b51df2d1a53889848937d3
marwan7878 <marawan20191700620@cis.asu.edu.eg>
2023-04-30 10:35:17 +0300

    modify style of table of index page of contactus

--- resources/views/ContactUs/index.blade.php
+++ resources/views/ContactUs/index.blade.php
@@ -12,8 +12,8 @@
     <table class="table" id="table">
           <thead style="border-bottom: #2f80ed 3px solid">
             <tr style="color: #2f80ed">
-              <th scope="col" style="width:70px">#</th>
-              <th style="width: 7rem" scope="col">الاسم الاول</th>
+              <th scope="col" style="width: 7rem;">#</th>
+              <th scope="col">الاسم الاول</th>
               <th scope="col">الاسم الثاني</th>
               <th scope="col">تاريخ الانشاء</th>
               <th scope="col">الخيارات</th>
@@ -24,14 +24,14 @@
                   $counter =1;
               @endphp
             @foreach ($all_messages as $message)
-            <tr class="search2" style="border-bottom: 1px double #5d657b">
-              <th scope="row" style="color: #2f80ed">{{$counter++}}</th>
-              <td><p class="ms-5 title" style="inline-size: 17rem; overflow-wrap: break-word">{{$message->first_name}}</p></td>
-              <td><p class="ms-5 title" style="inline-size: 17rem; overflow-wrap: break-word">{{$message->second_name}}</p></td>
-              <td><p class="ms-5 title" style="inline-size: 17rem; overflow-wrap: break-word">{{$message->created_at}}</p></td>
-              <td>
-                <a class="btn btn-secondary ms-1 py-1" href="{{ route('contactus.show', $message->id) }}">عرض</a> 
-                <a class="btn btn-danger ms-1 py-1" href="{{ route('contactus.soft_delete', $message->id) }}">حذف</a>  
+            <tr class="search2 " style="border-bottom: 1px double #5d657b">
+              <td scope="row" style="color: #2f80ed">{{$counter++}}</td>
+              <td style="max-width: 30px;word-wrap: break-word;padding-left: 40px;"><p class=" title" style=" overflow-wrap: break-word">{{$message->first_name}}</p></td>
+              <td style="max-width: 30px;word-wrap: break-word;padding-left: 40px;"><p class=" title" style=" overflow-wrap: break-word">{{$message->second_name}}</p></td>
+              <td class=""><p class=" title" style=" overflow-wrap: break-word;max-width: 85px;">{{$message->created_at}}</p></td>
+              <td class="">
+                <a class="btn btn-secondary  py-1 " href="{{ route('contactus.show', $message->id) }}">عرض</a> 
+                <a class="btn btn-danger  py-1 " href="{{ route('contactus.soft_delete', $message->id) }}">حذف</a>  
               </td>
             </tr>
                 
