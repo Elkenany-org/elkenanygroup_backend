69f5ebc1f3b9fd84c86f99dd0773bc703069b031
marwan7878 <marawan20191700620@cis.asu.edu.eg>
2023-04-30 15:36:45 +0300

    modify table style of news index

--- resources/views/News/index.blade.php
+++ resources/views/News/index.blade.php
@@ -22,16 +22,15 @@
     <table class="table" id="table">
           <thead style="border-bottom: #2f80ed 3px solid">
             <tr style="color: #2f80ed">
-              <th scope="col" style="width:60px">#</th>
-              <th style="width: 7rem" scope="col">الصورة</th>
-              <th class="news_sorting" data-sorting_type="asc" data-column_name="title" scope="col">العنوان</th>
-              <th class="news_sorting" data-sorting_type="asc" data-column_name="category" scope="col">القسم</th>
+              <th scope="col" style="width: 5rem;">#</th>
+              <th scope="col" style="width: 8rem;">الصورة</th>
+              <th scope="col">العنوان</th>
+              <th scope="col">القسم</th>
               <th scope="col">تاريخ الانشاء</th>
               <th scope="col">تاريخ التعديل</th>
               <th scope="col">الخيارات</th>
             </tr>
           </thead>
-            {{-- @include('News.rows_of_index') --}}
             <tbody id="tbody">
               @php
                     $counter =1;
@@ -40,10 +39,10 @@
               <tr class="search2" style="border-bottom: 1px double #5d657b">
                 <th scope="row" style="color: #2f80ed">{{$counter++}}</th>
                 <td><img src="images/news/{{$event->image}}" alt="error" style="width: 60px"></td>
-                <td><p class="ms-5 title" style="inline-size: 17rem; overflow-wrap: break-word">{{$event->title}}</p></td>
-                <td><p class="ms-5 title" style="inline-size: 17rem; overflow-wrap: break-word">{{$event->category->name_ar}}</p></td>
-                <td><p class="ms-5" style="inline-size: 7rem; overflow-wrap: break-word">{{($event->created_at)->format('d/m/Y   h:i:s')}}</p></td>
-                <td><p class="ms-5" style="inline-size: 7rem; overflow-wrap: break-word">{{($event->updated_at)->format('d/m/Y   h:i:s')}}</p></td>
+                <td style="max-width:  11rem;;word-wrap: break-word;padding-left: 40px;"><p class=" title" style=" overflow-wrap: break-word">{{$event->title}}</p></td>
+                <td style="max-width:  11rem;;word-wrap: break-word;padding-left: 90px;"><p class=" title" style=" overflow-wrap: break-word">{{$event->category->name_ar}}</p></td>
+                <td style="max-width:  7rem;;word-wrap: break-word;padding-left: 40px;"><p class=" title" style=" overflow-wrap: break-word">{{($event->created_at)->format('d/m/Y   h:i:s')}}</p></td>
+                <td style="max-width:  7rem;;word-wrap: break-word;padding-left: 40px;"><p class=" title" style=" overflow-wrap: break-word">{{($event->updated_at)->format('d/m/Y   h:i:s')}}</p></td>
                 <td>
                   <a class="btn btn-secondary ms-1 py-1" href="{{ route('News.edit', $event->id) }}">تعديل</a> 
                   <a class="btn btn-danger ms-1 py-1" href="{{ route('News.soft_delete', $event->id) }}">حذف</a>  
