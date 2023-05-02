0a9d669b183c03f217c6f2b23521b0daba12adcf
marwan7878 <marawan20191700620@cis.asu.edu.eg>
2023-05-02 14:34:14 +0300

    modify design of table of archive page of categories

--- resources/views/Categories/archive.blade.php
+++ resources/views/Categories/archive.blade.php
@@ -20,6 +20,8 @@
               <th scope="col" style="width:70px">#</th>
               <th style="width: 7rem" scope="col">الاسم AR</th>
               <th scope="col">الاسم EN </th>
+              <th scope="col">تاريخ الانشاء</th>
+              <th scope="col">تاريخ التعديل</th>
               <th scope="col">تاريخ الحذف</th>
               <th scope="col">الخيارات</th>
             </tr>
@@ -29,17 +31,18 @@
                   $counter =1;
               @endphp
             @foreach ($categories as $category)
-            <tr class="search2" style="border-bottom: 1px double #5d657b">
+            <tr style="border-bottom: 1px double #5d657b">
               <th scope="row" style="color: #2f80ed">{{$counter++}}</th>
-              <td><p class="ms-5 title" style="inline-size: 17rem; overflow-wrap: break-word">{{$category->name_ar}}</p></td>
-              <td><p class="ms-5 title" style="inline-size: 17rem; overflow-wrap: break-word">{{$category->name_en}}</p></td>
-              <td><p class="ms-5" style="inline-size: 7rem; overflow-wrap: break-word">{{($category->deleted_at)->format('d/m/Y   h:i:s')}}</p></td>
+              <td style="max-width:  11rem;word-wrap: break-word;padding-left: 40px;"><p class=" title" style=" overflow-wrap: break-word">{{$category->name_ar}}</p></td>
+              <td style="max-width:  11rem;word-wrap: break-word;padding-left: 40px;"><p class=" title" style=" overflow-wrap: break-word">{{$category->name_en}}</p></td>
+              <td style="max-width:  5rem;word-wrap: break-word;padding-left: 40px;"><p class=" title" style=" overflow-wrap: break-word">{{($category->created_at)->format('d/m/Y   h:i:s')}}</p></td>
+              <td style="max-width:  5rem;word-wrap: break-word;padding-left: 40px;"><p class=" title" style=" overflow-wrap: break-word">{{($category->updated_at)->format('d/m/Y   h:i:s')}}</p></td>
+              <td style="max-width:  5rem;word-wrap: break-word;padding-left: 40px;"><p class=" title" style=" overflow-wrap: break-word">{{($category->deleted_at)->format('d/m/Y   h:i:s')}}</p></td>
               <td>
                 <a class="btn btn-secondary ms-1 py-1" href="{{ route('category.restore', $category->id) }}">استرجاع</a> 
                 <a class="btn btn-danger ms-1 py-1" href="{{ route('category.hard_delete', $category->id) }}">حذف نهائي</a>  
               </td>
             </tr>
-                
             @endforeach
           </tbody>
     </table>  
