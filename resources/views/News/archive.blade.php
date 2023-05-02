a0ea136a12b6d9a7e37cd0a839d9342e923f0d43
marwan7878 <marawan20191700620@cis.asu.edu.eg>
2023-05-02 15:07:13 +0300

    modify design of table of archive page of news

--- resources/views/News/archive.blade.php
+++ resources/views/News/archive.blade.php
@@ -17,12 +17,13 @@
   <table class="table">
         <thead style="border-bottom: #2f80ed 3px solid">
           <tr style="color: #2f80ed">
-            <th scope="col" style="width: 5rem;">#</th>
+              <th scope="col" style="width: 5rem;">#</th>
               <th scope="col" style="width: 8rem;">الصورة</th>
               <th scope="col">العنوان</th>
               <th scope="col">القسم</th>
               <th scope="col">تاريخ الانشاء</th>
               <th scope="col">تاريخ التعديل</th>
+              <th scope="col">تاريخ الحذف</th>
               <th scope="col">الخيارات</th>
           </tr>
         </thead>
@@ -35,14 +36,15 @@
             <th scope="row" style="color: #2f80ed ">{{$counter++}}</th>
             <td><img src="/images/news/{{$event->image}}" alt="error" style="width: 60px"></td>
             <td style="max-width:  11rem;word-wrap: break-word;padding-left: 40px;"><p class=" title" style=" overflow-wrap: break-word">{{$event->title}}</p></td>
-            <td style="max-width:  11rem;word-wrap: break-word;padding-left: 40px;"><p class=" title" style=" overflow-wrap: break-word">{{$event->category->name_ar}}</p></td>
+            <td style="max-width:  11rem;word-wrap: break-word;padding-left: 40px;"><p class=" title" style=" overflow-wrap: break-word; padding-left: 30px;">{{$event->category->name_ar}}</p></td>
             <td style="max-width:  7rem;word-wrap: break-word;padding-left: 40px;"><p class=" title" style=" overflow-wrap: break-word">{{($event->created_at)->format('d/m/Y   h:i:s')}}</p></td>
             <td style="max-width:  7rem;word-wrap: break-word;padding-left: 40px;"><p class=" title" style=" overflow-wrap: break-word">{{($event->updated_at)->format('d/m/Y   h:i:s')}}</p></td>
+            <td style="max-width:  7rem;word-wrap: break-word;padding-left: 40px;"><p class=" title" style=" overflow-wrap: break-word">{{($event->deleted_at)->format('d/m/Y   h:i:s')}}</p></td>
             <td>
               <a class="btn btn-primary ms-1 py-1" href="{{ route('News.restore', $event->id) }}">استرجاع</a>
               <a class="btn btn-danger ms-1 py-1" href="{{ route('News.hard_delete', $event->id) }}">حذف نهائي</a>  
             </td>
-           </tr>
+          </tr>
               
           @endforeach
         </tbody>
