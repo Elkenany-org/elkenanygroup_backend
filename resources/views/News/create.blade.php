01662df27a5c82ae3552a9193cff205734cbff1d
marwan7878 <marawan20191700620@cis.asu.edu.eg>
2023-05-03 15:28:55 +0300

    modify design of create page of contactus

--- resources/views/News/create.blade.php
+++ resources/views/News/create.blade.php
@@ -2,22 +2,54 @@
 
 @section('content')
 
-<div class="p-3">
-    <form action="{{route('News.store')}}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <label for="fname">العنوان: </label>
-        <input type="text" id="fname" name="title"><br><br>
-        <label for="fname">القسم: </label>
-        <select name="category_id" class="form-control w-25">
-          @foreach($categories as $category)
-              <option value="{{ $category->id }}">{{ $category->name_ar }}</option>
-          @endforeach
-        </select><br>
-        <label for="lname">الوصف: </label>
-        <textarea name="description"></textarea>
-        <input type="file" id="file" name="image">
-        <input type="submit" value="Submit">
-    </form> 
+<div class="card-styles">
+  <div class="card-style-3 mb-30">
+      <div class="card-content">            
+          <div class="row">
+            <form action="{{route('News.store')}}" method="POST" enctype="multipart/form-data">
+              @csrf
+
+              <div class="col-12">
+                <div class="input-style-1">
+                  <label for="name">العنوان</label>
+                  <input type="text" class="form-control" name="email" id="name">
+                </div>
+              </div>
+              <div class="col-12">
+                <div class="input-style-1">
+                  <label for="name">القسم</label>
+                  <select name="category_id" class="form-control w-25">
+                    @foreach($categories as $category)
+                        <option value="{{ $category->id }}">{{ $category->name_ar }}</option>
+                    @endforeach
+                  </select>  
+                </div>
+              </div>
+              <div class="col-12">
+                <div class="input-style-1">
+                  <label for="name">الرسالة</label>
+                  <textarea name="description"></textarea>
+                </div>
+              </div>
+              
+              <div class="col-12">
+                <div class="input-style-1">
+                  <label for="name">الصورة</label>
+                  <input type="file" class="file" id="file" name="image">
+                </div>
+              </div>
+              
+              <div class="col-12">
+                  <div class="button-group d-flex justify-content-center flex-wrap">
+                    <input class="main-btn primary-btn btn-hover w-25 text-center" type="submit" value="Submit">
+                  </div>
+              </div>
+              </div>
+            </form> 
+      </div>
+  </div>
+</div>
+
 </div>
 
     <script>
