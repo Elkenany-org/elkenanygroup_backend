7ff66d6cb164d7b4e030f3e140a3d73925a58515
marwan7878 <marawan20191700620@cis.asu.edu.eg>
2023-05-17 17:52:32 +0300

    modifications

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -89,7 +89,11 @@ class CategoryController extends Controller
     }
     public function archive_search(Request $request)
     {
-        
+        if($request->name == null)
+        {
+            $categories = Category::onlyTrashed()->paginate(10);
+            return view('Categories.archive')->with('categories',$categories);
+        }
         
         $name = $request->name;
         $categories = Category::onlyTrashed()->where('name_ar', 'LIKE', '%'.$name.'%')
